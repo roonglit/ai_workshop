3711d4c608a7a1232a219e22f1f5b6fee4b3fd19
Roonglit <roonglit@gmail.com>
2026-03-10 13:31:05 +0700

    add product search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all.order(:category, :name)
+    @products = Product.all
+    if params[:query].present?
+      q = "%#{params[:query]}%"
+      @products = @products.where("name LIKE ? OR description LIKE ? OR tags LIKE ?", q, q, q)
+    end
+    @products = @products.order(:category, :name)
   end
 end
